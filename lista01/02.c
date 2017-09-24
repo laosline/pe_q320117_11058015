bd7995578a641a54c86cd594145672eb063435af
Laosline <lhgois.campos@hotmail.com>
2017-09-24 18:41:42 -0300

    adicionado exercicio 02

---
+++ lista01/02.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+	
+	int x, y, z;
+
+	printf("Insira o primeiro numero: ");
+	scanf("%d", &x);
+
+	printf("Insira o segundo numero: ");
+	scanf("%d", &y);
+
+	z=x+y;
+	
+	printf("A soma dos numeros inseridos e: %d", z);
+
+}
