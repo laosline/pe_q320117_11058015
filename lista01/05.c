7338feefeb30e0732f552db05d5b53b579df6492
Laosline <lhgois.campos@hotmail.com>
2017-09-24 18:49:35 -0300

    adicionado os exercicios 03, 04 e 05

---
+++ lista01/05.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+	
+	int x, y, z;
+	
+	printf("Insira o primeiro numero: ");
+	scanf("%d", &x);
+
+	printf("Insira o segundo numero: ");
+	scanf("%d", &y);
+
+	z=x/y;
+	
+	printf("A divisao dos numeros inseridos e: %d", z);
+
+}
