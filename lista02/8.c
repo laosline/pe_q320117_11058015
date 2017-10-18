1d94f8b8c927dc33930878d003a2ef54d69079e8
Laosline <lhgois.campos@hotmail.com>
2017-10-17 22:27:22 -0200

    Adicionado exercicio 8

---
+++ lista02/8.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+double desconto(int f, int h, double s)
+{
+	return f*(s/20);
+}
+double acrescimo(int f, int h, double s)
+{
+	double a;
+	if (h<=40){
+		a=((s/160)+40)*h;
+	} else {
+		a=((s/160)+40)*40;
+	}
+	return a;
+}
+double contagem(int f, int h, double s)
+{
+	double res;
+	res=s+acrescimo(f,h,s)-desconto(f,h,s);
+	return res;
+}
+
+double salario (int cargo, int f, int h)
+{
+	switch(cargo)
+	{
+		case 1 :
+			return contagem(f,h,10000);
+			break;
+		case 2 :
+			return contagem(f,h,8000);
+			break;
+		case 3 :
+			return contagem(f,h,5000);
+			break;
+		case 4 :
+			return contagem(f,h,3000);
+			break;
+		case 5 :
+			return contagem(f,h,2000);
+			break;
+	}
+}
+	
+main(){
+	int cargo, faltas, horas_extras;
+	scanf("%d %d %d", &cargo, &faltas, &horas_extras);
+	printf("%.0lf", salario(cargo,faltas,horas_extras));
+}
